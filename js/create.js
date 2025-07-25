64c0ea5ee90fa58dc11e911cdea86542c8e0d1e2
Nebulocity <Nebulocity@users.noreply.github.com>
2025-07-25 13:31:38 -0700

    Update create.js

--- js/create.js
+++ js/create.js
@@ -1,12 +1,8 @@
 function gameCreate() {
 
-	var events;
-	var star, Material;
-	var starGrav = 10; 
-	var starTemp = 10;
-	var starRadius = 10;
-	var starPressure = 10;
-	
+	var events, star;
+	const starState = this.starState;
+
 	var centerX = this.sys.game.config.width / 2;
 	var centerY = this.sys.game.config.height / 2;
 	
@@ -29,11 +25,26 @@ function gameCreate() {
     this.space_layer3 = this.add.tileSprite(0, 0, 2400, 900, 'space_layer3').setScale(1);
     this.space_layer1.depth = -97;
 
-	var star = this.add.circle(centerX, centerY, starRadius, 0xFFFF00); // 0xFF0000 is red
+	// Add status text
+	this.statusText = this.add.text(20, 20, '', { fontSize: '16px', fill: '#fff' });
+
+	// Star state for evolving stars
+	this.starState = {
+		mass:.08,
+		temperature: 1000000,
+		gravity: 1,
+		pressure: 1,
+		radius: 10,
+		phase: 'Protostar',
+		lifetime: 0
+	};
+
+	// Create the star
+	this.star = this.add.circle(centerX, centerY, this.starState.radius, 0xFFFF00); // 0xFF0000 is red
 
 	// Create the pulsing tween
 	this.tweens.add({
-		targets: star,
+		targets: this.star,
 		scaleX: 0.95, // Shrink to 80% of original width
 		scaleY: 0.95, // Shrink to 80% of original height
 		yoyo: true, // Go back to original size after shrinking
@@ -42,70 +53,36 @@ function gameCreate() {
 		duration: 1500 // Tween duration (milliseconds)
 	});
 	
-	// Add material button
-	const addGasButton = this.add.image(centerX + 300, centerY + 125, 'gasButton');
-	addGasButton.setScale(.1);
-	addGasButton.setInteractive({ useHandCursor: true });
-	addGasButton.on('pointerdown', () => clickAddGasButton());
 	
-	var addGasText = this.add.text(addGasButton.x - 60, addGasButton.y - 10, '+ Material', {
+	// Add material button
+	const AddMaterialButton = this.add.image(centerX + 300, centerY + 125, 'gasButton');
+	AddMaterialButton.setScale(.1);
+	AddMaterialButton.setInteractive({ useHandCursor: true });
+	AddMaterialButton.on('pointerdown', () => { 
+		console.log("adding material!");
+		this.starState.mass += 1 
+	});
+	
+	var AddMaterialText = this.add.text(AddMaterialButton.x - 60, AddMaterialButton.y - 10, '+ Material', {
 		fontSize: '18px',
 		fille: '#ffffff'
 	});
 
 	
 	// Sub Material button
-	const subGasButton = this.add.image(centerX + 300, centerY + 175, 'gasButton');
-	subGasButton.setScale(.1);
-	subGasButton.setInteractive({ useHandCursor: true });
-	subGasButton.on('pointerdown', () => clickSubGasButton());
+	const SubMaterialButton = this.add.image(centerX + 300, centerY + 175, 'gasButton');
+	SubMaterialButton.setScale(.1);
+	SubMaterialButton.setInteractive({ useHandCursor: true });
+	SubMaterialButton.on('pointerdown', () => {
+		console.log("subtracting material!");
+		this.starState.mass = Math.max(1, this.starState.mass - 1); 
+	});
 	
-	var subGasText = this.add.text(subGasButton.x - 60, subGasButton.y - 10, '- Material', {
+	var SubMaterialText = this.add.text(SubMaterialButton.x - 60, SubMaterialButton.y - 10, '- Material', {
 		fontSize: '18px',
 		fille: '#ffffff'
 	});
 	
-	
-	
-	
-    // Create a random event
-    // events = this.time.addEvent({
-        // delay: Phaser.Math.Between(1, 1000),
-        // loop: true,
-        // callback: createEvent,
-        // callbackScope: this,
-        // args: [this]
-    // });
-}
-
-function clickAddGasButton() {
-	console.log('Adding Material!');
-}
-
-function clickSubGasButton() {
-	console.log('Subtracting Material!');
-}
-
-	
-function createEvent(scene) {
-
-        console.log("game update event");
-
-        // for (var i = 0; i < 3; i++) {
-			
-			// console.log(i);
-			
-            // var newAsteroid = this.physics.add.sprite(Math.floor(Math.random() * (1500 - 1 + 1300) + 1300), Math.floor(Math.random() * (400 - 1 + 25) + 25), 'asteroid');
-            // var depth = Math.round(Math.random());
-            // if (depth == 0) { depth = -1 };
-
-            // newAsteroid.depth = depth;
-
-            // asteroids.add(newAsteroid);
-            // newAsteroid.play('spin');
-
-        // }
-    // }
 }
 
 export default gameCreate;
