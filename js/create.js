edf07cb39f72529d79b0846da63308e225548f10
Nebulocity <Nebulocity@users.noreply.github.com>
2025-08-01 12:19:37 -0700

    Update create.js

--- js/create.js
+++ js/create.js
@@ -61,7 +61,7 @@ function gameCreate() {
 
 	// Star initial state
 	this.starState = {
-		phase: 'Hydrogen Atoms',
+		phase: 'Hydrogen',
 		status: 'Stable',
 		mass: mass,
 		volume: volume,
@@ -76,20 +76,34 @@ function gameCreate() {
 		lifetime: 0
 	};
 
-	showInfo(this, "Hydrogen atoms are appearing in this area!");
+	showInfo(this, "Hydrogen is appearing in this area!");
 	
 	// Hydrogen atom emitter at start
-	this.atomRect = new Phaser.Geom.Rectangle(0, 0, 800, 450); 
+	this.atoms = new Phaser.Geom.Rectangle(0, 0, 800, 450); 
 	this.atomEmitter = this.add.particles(0, 0, 'hydrogen', {
-		emitZone: { source: this.atomRect, type: 'random' }, 
+		emitZone: { source: this.atoms, type: 'random' }, 
 		scale: { start: 0.0025, end: 0 },
+		emitting: true
+	});
+	
+	// Hydrogen condensing
+	this.hydrogenCondensing = new Phaser.Geom.Circle(centerX, centerY, ((centerX + centerY) / 2)); 
+	this.hydrogenCondensingEmitter = this.add.particles(0, 0, 'hydrogen', { 
+		emitZone: { source: this.hydrogenCondensing }, 
+		moveToX: centerX, // Particles will move towards this X coordinate
+		moveToY: centerY, // Particles will move towards this Y coordinate
+		lifespan: 5000,
+		speed: { min: 2, max: 10 },
+		scale: { start: 0.0025, end: 0 },
+		gravityY: 0,
+		blendMode: 'ADD',
 		emitting: false
 	});
 	
-	// Hydrogen atoms condensing
-	this.atomCircle = new Phaser.Geom.Circle(centerX, centerY, ((centerX + centerY) / 4)); 
-	this.atomCondensingEmitter = this.add.particles(0, 0, 'hydrogen', { 
-		emitZone: { source: this.atomCircle }, 
+	// Hydrogen critical collapse
+	this.cloudCondensing = new Phaser.Geom.Circle(centerX, centerY, ((centerX + centerY) / 4)); 
+	this.cloudCondensingEmitter = this.add.particles(0, 0, 'hydrogen', { 
+		emitZone: { source: this.cloudCondensing }, 
 		moveToX: centerX, // Particles will move towards this X coordinate
 		moveToY: centerY, // Particles will move towards this Y coordinate
 		lifespan: 5000,
@@ -101,18 +115,26 @@ function gameCreate() {
 	});
 	
 	// Molecular cloud emitter
-	this.cloud = new Phaser.Geom.Circle(centerX, centerY, ((centerX + centerY) / 8));
-	this.cloudEmitter = this.add.particles(0, 0, 'hydrogen', { 
-		emitZone: { source: this.cloud }, 
+	this.cloudCriticalCondensing = new Phaser.Geom.Circle(centerX, centerY, ((centerX + centerY) / 4));
+	this.cloudCriticalCondensingEmitter = this.add.particles(0, 0, 'hydrogen', { 
+		emitZone: { source: this.cloudCriticalCondensing }, 
 		moveToX: centerX, // Particles will move towards this X coordinate
 		moveToY: centerY, // Particles will move towards this Y coordinate
 		lifespan: 4000,
-		speed: { min: 200, max: 400 },
+		speed: { min: 100, max: 800 },
 		scale: { start: 0.0025, end: 0 },
 		gravityY: 0,
 		blendMode: 'ADD',
 		emitting: false
 	});
+
+	// Creates a dramatic effect to indicate the birth of the protostar
+	this.flashRect = this.add.rectangle(0, 0, 800, 450, 0xffffff)
+		.setOrigin(0, 0)
+		.setAlpha(0)
+		.setDepth(10);
+
+
 
 	// Protostar emitters
 	// this.star = new Phaser.Geom.Circle(centerX, centerY, ((centerX + centerY) / 2));
@@ -134,11 +156,10 @@ function gameCreate() {
 		lifespan: 600,
 		speed: { min: 50, max: 100 },
 		scale: { start: 0.0025, end: 0 },
-		radius: 50,
 		quantity: 100,
 		gravityY: 0,
 		blendMode: 'ADD',
-		emitting: true
+		emitting: false
 	});
 	
 	
@@ -169,7 +190,7 @@ function gameCreate() {
 	addBtn.on('pointerdown', () => {
 
 		// var amountToAdd = Math.random() * (10e9 - 5e9 + 10e9) + 5e9;
-		var amountToAdd = Math.random() * (1 - .01) + .01;
+		var amountToAdd = Math.random() * (1e5 - .01) + .01;
 		
 		this.tweens.add({
 			targets: this.starState,
@@ -182,6 +203,52 @@ function gameCreate() {
 	});
 	
 	this.add.text(addBtn.x - 60, addBtn.y - 10, '+ Hydrogen', { fontSize: '18px', fill: '#ffffff' });
+	
+	
+	
+	// Status bar background
+	this.statusBar = this.add.rectangle(0, 0, 900, 65, 0x000000, 0.6).setOrigin(0, 0).setDepth(100);
+	
+	// Icon spacing and sizeToContent
+	const iconSize = 24;
+	const paddingX = 15;
+	const spacingX = 125;
+	
+	// Vertical centering on bar.  Change back to 20 later.
+	const iconY = 40; 
+	
+	// Temporary: for putting a header line above the text until I get icons.
+	const textY = 20;
+	
+	// Icons (placeholders)
+	// this.icons = {
+		// lifetime: this.add.image(paddingX, iconY, '').setScale(0.075).setDepth(101), radius: this.add.image(paddingX + spacingX, iconY),
+		// mass: this.add.image(paddingX + spacingX + 30, iconY, '').setScale(.015).setDepth(101), radius: this.add.image(paddingX + spacingX * 2, iconY),
+		// density: this.add.image(paddingX + spacingX * 2 + 30, iconY, '').setScale(0.075).setDepth(101), radius: this.add.image(paddingX + spacingX * 3, iconY),
+		// radius: this.add.image(paddingX + spacingX * 3 + 45, iconY, '').setScale(0.075).setDepth(101), radius: this.add.image(paddingX + spacingX * 4, iconY),
+		// temp: this.add.image(paddingX + spacingX * 4 + 45, iconY, '').setScale(0.075).setDepth(101), radius: this.add.image(paddingX + spacingX * 5, iconY),
+		// phase: this.add.image(paddingX + spacingX * 5 + 30, iconY, '').setScale(0.075).setDepth(101), radius: this.add.image(paddingX + spacingX * 6, iconY),
+	// };
+	
+	// Temporary: for putting a header line above the text until I get icons.
+	this.headerTexts = {
+		lifetime: this.add.text(paddingX + 30, textY, 'Lifetime', {fontSize:'14px', color:'#FFFFFF'}).setOrigin(0, 0.5).setDepth(101),
+		mass: this.add.text(paddingX + spacingX + 30, textY, 'Mass', {fontSize:'14px', color:'#FFFFFF'}).setOrigin(0, 0.5).setDepth(101),
+		density: this.add.text(paddingX + spacingX * 2 + 60, textY, 'Density', {fontSize:'14px', color:'#FFFFFF'}).setOrigin(0, 0.5).setDepth(101),
+		radius: this.add.text(paddingX + spacingX * 3 + 85, textY, 'Radius', {fontSize:'14px', color:'#FFFFFF'}).setOrigin(0, 0.5).setDepth(101),
+		temp: this.add.text(paddingX + spacingX * 4 + 85, textY, 'Temp', {fontSize:'14px', color:'#FFFFFF'}).setOrigin(0, 0.5).setDepth(101),
+		phase: this.add.text(paddingX + spacingX * 5 + 85, textY, 'Phase', {fontSize:'14px', color:'#FFFFFF'}).setOrigin(0, 0.5).setDepth(101)
+	};
+	
+	// Text labels next to icons
+	this.statusTexts = {
+		lifetime: this.add.text(paddingX + 30, iconY, '', {fontSize:'14px', color:'#FFFFFF'}).setOrigin(0, 0.5).setDepth(101),
+		mass: this.add.text(paddingX + spacingX + 30, iconY, '', {fontSize:'14px', color:'#FFFFFF'}).setOrigin(0, 0.5).setDepth(101),
+		density: this.add.text(paddingX + spacingX * 2 + 60, iconY, '', {fontSize:'14px', color:'#FFFFFF'}).setOrigin(0, 0.5).setDepth(101),
+		radius: this.add.text(paddingX + spacingX * 3 + 85, iconY, '', {fontSize:'14px', color:'#FFFFFF'}).setOrigin(0, 0.5).setDepth(101),
+		temp: this.add.text(paddingX + spacingX * 4 + 85, iconY, '', {fontSize:'14px', color:'#FFFFFF'}).setOrigin(0, 0.5).setDepth(101),
+		phase: this.add.text(paddingX + spacingX * 5 + 85, iconY, '', {fontSize:'14px', color:'#FFFFFF'}).setOrigin(0, 0.5).setDepth(101)
+	};
 }
 
 
